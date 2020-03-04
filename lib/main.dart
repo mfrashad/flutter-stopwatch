11c9eecfc194c616a2dac27ac1d524b422766c78
Muhammad Fathy Rashad <m.fathyrashad@gmail.com>
2020-03-04 13:44:47 +0800

    Add functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +8,60 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'STOPWATCH',
-      home: Scaffold(
+      home: StopwatchApp()
+    );
+  }
+}
+
+class StopwatchApp extends StatefulWidget {
+  @override
+  _StopwatchAppState createState() => _StopwatchAppState();
+}
+
+class _StopwatchAppState extends State<StopwatchApp> {
+
+  String timeString = "00:00:00";
+  Stopwatch stopwatch = Stopwatch();
+  Timer timer;
+
+  void start(){
+    stopwatch.start();
+    timer = Timer.periodic(Duration(milliseconds: 1), update);
+  }
+
+  void update(Timer t){
+    if(stopwatch.isRunning){
+      setState(() {
+        timeString =
+            (stopwatch.elapsed.inMinutes % 60).toString().padLeft(2, "0") + ":" +
+                (stopwatch.elapsed.inSeconds % 60).toString().padLeft(2, "0") + ":" +
+                (stopwatch.elapsed.inMilliseconds % 1000 / 10).clamp(0, 99).toStringAsFixed(0).padLeft(2, "0");
+      });
+
+    }
+  }
+
+  void stop(){
+    setState(() {
+      timer.cancel();
+      stopwatch.stop();
+    });
+
+  }
+
+  void reset(){
+    timer.cancel();
+    stopwatch.reset();
+    setState((){
+      timeString = "00:00:00";
+
+    });
+    stopwatch.stop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         backgroundColor: Colors.grey.shade200,
         body: Column(
           children: <Widget>[
@@ -43,10 +97,10 @@ class MyApp extends StatelessWidget {
                   children: <Widget>[
                     Icon(Icons.timer, size: 90, color: Colors.grey.shade900),
                     Text("00:00:00",
-                      style: TextStyle(
-                        fontSize: 40,
-                        color: Colors.grey.shade900
-                      )
+                        style: TextStyle(
+                            fontSize: 40,
+                            color: Colors.grey.shade900
+                        )
                     )
                   ],
                 ),
@@ -58,25 +112,25 @@ class MyApp extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
                   FlatButton(
-                    onPressed: (){},
-                    child: Container(
-                      height: 100,
-                      width: 100,
-                      decoration: BoxDecoration(
-                          color: Colors.grey.shade200,
-                          shape: BoxShape.circle,
-                          boxShadow: [
-                            BoxShadow(
-                                offset: Offset(10, 10),
-                                color: Colors.black38,
-                                blurRadius: 15),
-                            BoxShadow(
-                                offset: Offset(-10, -10),
-                                color: Colors.white.withOpacity(0.85),
-                                blurRadius: 15)
-                          ]),
-                      child: Icon(Icons.refresh, size: 60),
-                    )
+                      onPressed: (){},
+                      child: Container(
+                        height: 100,
+                        width: 100,
+                        decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            shape: BoxShape.circle,
+                            boxShadow: [
+                              BoxShadow(
+                                  offset: Offset(10, 10),
+                                  color: Colors.black38,
+                                  blurRadius: 15),
+                              BoxShadow(
+                                  offset: Offset(-10, -10),
+                                  color: Colors.white.withOpacity(0.85),
+                                  blurRadius: 15)
+                            ]),
+                        child: Icon(Icons.refresh, size: 60),
+                      )
                   ),
                   FlatButton(
                       onPressed: (){},
@@ -104,8 +158,6 @@ class MyApp extends StatelessWidget {
             )
           ],
         )
-      ),
     );
   }
 }
-
