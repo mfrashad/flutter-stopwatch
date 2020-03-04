841822bad7ebd49a8c4dff06738ec57c9c7a926e
Muhammad Fathy Rashad <m.fathyrashad@gmail.com>
2020-03-04 08:18:49 +0800

    Add Counter Container

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'STOPWATCH',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: const Text('Hello World'),
-        ),
+        backgroundColor: Colors.grey.shade200,
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 60),
+              child: Text("STOPWATCH",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey.shade900,
+                  )
+              ),
+            ),
+            Container(
+              width: 280,
+              height: 280,
+              decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(10, 10),
+                        color: Colors.black38,
+                        blurRadius: 15),
+                    BoxShadow(
+                        offset: Offset(-10, -10),
+                        color: Colors.white.withOpacity(0.85),
+                        blurRadius: 15)
+                  ]),
+            )
+          ],
+        )
       ),
     );
   }
 }
+
